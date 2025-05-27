5193f9fc4e6ff034ee298d53d1f9f4146f1cfeaf
EnzoLavieri <devenzolavieri@gmail.com>
2025-05-27 14:25:22 -0300

    delete by id

--- estudo-adonis/app/Controllers/Http/MedicamentsController.ts
+++ estudo-adonis/app/Controllers/Http/MedicamentsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Medicament from "App/Models/Medicament";
+import databaseConfig from "Config/database";
 
 //implementar depois erros e exceções
 //https://legacy.adonisjs.com/docs/3.2/error-and-exceptions
@@ -16,7 +17,7 @@ export default class MedicamentsController {
 
     return response.json({
       message: "Medicamento criado com sucesso!",
-      data: medicament,
+      medicament: medicament,
     });
   }
 
@@ -25,7 +26,7 @@ export default class MedicamentsController {
     const medicaments = await Medicament.all();
 
     return response.json({
-      data: medicaments,
+      medicaments: medicaments,
     });
   }
 
@@ -34,7 +35,18 @@ export default class MedicamentsController {
     const medicament = await Medicament.findOrFail(params.id);
 
     return response.json({
-      data: medicament,
+      medicament: medicament,
+    });
+  }
+
+  //exclui um medicamento por id
+  public async destroy({ params, response }: HttpContextContract) {
+    const medicament = await Medicament.findOrFail(params.id);
+
+    await medicament.delete();
+
+    return response.json({
+      message: `Medicamento ${medicament.name} excluído com sucesso.`,
     });
   }
 }
